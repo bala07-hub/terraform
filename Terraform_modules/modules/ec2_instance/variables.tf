1e95c8df9e7bac4229386341ff3e9cd724aadd2f
Balamanikanta Anantha <bala@Balas-MacBook-Pro.local>
2026-02-07 12:18:02 -0600

    modularized the terraform project

---
+++ Terraform_modules/modules/ec2_instance/variables.tf
@@ -0,0 +1,11 @@
+variable "ami_value" {
+  description = "ami value of an ec2 instance"
+}
+variable "instance_type" {
+  description = "ec-2 instance type"
+
+}
+
+variable "subnet_id" {
+  description = "value of subnet_id"
+}
