1605197d74d07242c20c3db627b3b8df311cd2a5
Balamanikanta Anantha <bala@Balas-MacBook-Pro.local>
2026-02-08 07:05:00 -0600

    updated s3 bucket changes in backend

--- Terraform_Statefile/backend.tf
+++ Terraform_Statefile/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "bala-s3-demo"
-    key            = "bala/terraform.tfstate"
+    bucket         = "bala-s3-demo-test1"
     region         = "us-east-1"
+    key            = "bala/terraform.tfstate"
     encrypt        = true
     dynamodb_table = "terraform-lock"
   }
